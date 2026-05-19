81bffc02a6ddc63ef3accf2ca61b49df445fe5d9
Jeevash Khatri <jeevashkhatri676@gmail.com>
2026-05-19 21:24:14 +0500

    fix tanstack build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
+<<<<<<< HEAD
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()]
 })
+=======
+import tsConfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react(), tsConfigPaths()],
+})
+>>>>>>> fc93640 (vercel routing fix)
